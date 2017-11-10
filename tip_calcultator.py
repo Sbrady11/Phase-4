a7b5a78f464d4859070173bbdf013ef2de292462
Simon Brady <simonbrady@mac.com>
2017-11-10 16:07:21 -0500

    Update tip_calcultator.py
    Add updated code

--- tip_calcultator.py
+++ tip_calcultator.py
@@ -7,3 +7,20 @@ total = meal + meal * tip
 
 print("%.2f" % total)
 # this is how to print out a formatted total with only two decimal places
+
+-------------------------------------------------------------------------
+def tax(bill):
+  """Adds 8% tax to a restaurant bill."""
+  bill *= 1.08
+  print "With tax: %f" % bill
+  return bill
+
+def tip(bill):
+  """Adds 15% tip to a restaurant bill."""
+  bill *= 1.15
+  print "With tip: %f" % bill
+  return bill
+  
+meal_cost = 100
+meal_with_tax = tax(meal_cost)
+meal_with_tip = tip(meal_with_tax)
