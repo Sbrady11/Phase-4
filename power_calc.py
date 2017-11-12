3f5d93db819de3887938b630501f357e5d181801
Simon Brady <simonbrady@mac.com>
2017-11-12 12:13:07 -0500

    Update power_calc.py
    add cube function with control flow practice

--- power_calc.py
+++ power_calc.py
@@ -2,4 +2,14 @@ def power(base, exponent):
   result = base ** exponent
   print "%d to the power of %d is %d." % (base, exponent, result)
 
-power(37, 4)
+#####################################################################
+
+def cube(number):
+  return number ** 3
+
+def by_three(number):
+  if number % 3 == 0:
+  	return cube(number)
+  else:
+    return False
+  
