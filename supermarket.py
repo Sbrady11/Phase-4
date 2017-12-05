2d7caeac0e610211bf3f6770c461fe0dd4b57abd
Simon Brady <simonbrady@mac.com>
2017-12-04 21:09:32 -0500

    Update supermarket.py

--- supermarket.py
+++ supermarket.py
@@ -1,3 +1,5 @@
+#Practice with using dictionries and lists/accessing and manipulating info
+
 prices = {"banana": 4,"apple": 2,"orange": 1.5,"pear": 3}
 
 stock = {"banana": 6, "apple": 0, "orange": 32, "pear": 15}
