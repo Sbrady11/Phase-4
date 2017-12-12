074768be4c2208f715e475b95c471ed604f55cfe
Simon Brady <simonbrady@mac.com>
2017-12-11 22:00:30 -0500

    Update fizzbuzz.py

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -5,3 +5,13 @@ def fizz_count(string):
     	count += 1
   return count
     
+def fizz_buzz(num):
+  i = 0
+  for value in num: 
+    if value % 3 == 0 and value % 5 == 0:
+      print "fizzbuzz"
+    elif value % 3 == 0:
+      print "fizz"
+    elif value % 5 == 0:
+      print "buzz"
+      
