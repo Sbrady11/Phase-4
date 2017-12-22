a8ce02a62e71ca5d6ed2f1361951a52ebbb0f6b8
Simon Brady <simonbrady@mac.com>
2017-12-21 23:45:54 -0500

    Update fizzbuzz.py

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -5,14 +5,6 @@ def fizz_count(string):
     	count += 1
   return count
     
-def fizz_buzz(num):
-  i = 0
-  for value in num: 
-    if value % 3 == 0 and value % 5 == 0:
-      print "fizzbuzz"
-    elif value % 3 == 0:
-      print "fizz"
-    elif value % 5 == 0:
-      print "buzz"
-    else:
-      print value
+def fizz_buzz(number):
+  print("\n".join(["Fizz"*(i%3==0)+"Buzz"*(i%5==0) or str(i) for i in range(1,number + 1)]))
+  
